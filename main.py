62224b1518d76847590e5a8bad536c50fad2c75a
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2022-07-26 00:53:25 +0530

    commit 2
    features included:
    1) fast api functions for transition nodes created.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import xml.etree.ElementTree as ET
 from fastapi import FastAPI, UploadFile, File, Form, Request, Response
 from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
+from pip._vendor.requests.packages import package
 import os
 import matplotlib.pyplot as plt
 from fastapi.responses import FileResponse
-
 
 app = FastAPI()
 
@@ -27,10 +27,71 @@ async def create_upload_file(projectName: str, file: UploadFile, request):
     path = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename)
     await save_upload_file(file, path)
     df = pd.read_excel(path)
-    print(df)
+    # print(df)
     data = df.to_json(orient='split')
-    print(data)
+    # print(data)
     return {"data": data}
+
+@app.post("/convertxlsxtocsv/")
+async def create_upload_file(projectName: str, file: UploadFile, request):
+    ts = datetime.datetime.now().strftime("%m%d_%H%M%S")
+    path = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename)
+    await save_upload_file(file, path)
+    path2 = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename+".csv")
+    df = pd.read_excel(path)
+    # print(df)
+    df.to_csv(path2)
+    data = df.to_json(orient='split')
+    # print(data)
+    return {"data": data}
+
+@app.post("/convertjsontoxml/")
+async def create_upload_file(projectName: str, file: UploadFile, request):
+    ts = datetime.datetime.now().strftime("%m%d_%H%M%S")
+    path = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename)
+    await save_upload_file(file, path)
+    df = pd.read_json(path, orient='index')
+    path2 = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename+".xml")
+    df.to_xml(path2)
+    data = df.to_json(orient='split')
+    return {"data": data}
+
+@app.post("/convertxmltoxlsx/")
+async def create_upload_file(projectName: str, file: UploadFile, request):
+    ts = datetime.datetime.now().strftime("%m%d_%H%M%S")
+    path = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename)
+    await save_upload_file(file, path)
+    xml_data = open(path, 'r').read()
+    root = ET.XML(xml_data)  # Parse XML
+
+    data = []
+    cols = []
+    for i, child in enumerate(root):
+        data.append([subchild.text for subchild in child])
+        cols.append(child.tag)
+
+    df = pd.DataFrame(data).T  # Write in DF and transpose it
+    df.columns = cols  # Update column names
+    path2 = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename+".xlsx")
+
+    # print(df)
+    df.to_excel(path2)
+    data = df.to_json(orient='split')
+    # print(data)
+    return {"data": data}
+
+@app.post("/convertcsvtojson/")
+async def create_upload_file(projectName: str, file: UploadFile, request):
+    ts = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+    path = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename)
+    await save_upload_file(file, path)
+    path2 = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename + ".json")
+    # csvpath = Path('D:/output/'  + ts + '_' + file.filename + ".json")
+    csvfile= file.filename
+    df = pd.read_csv(path)
+    df.to_json(path2)
+    data = df.to_json(orient='split')
+    return {"filename": file.filename,"csvdata": data}
 
 @app.post("/uploadXmlFile/")
 async def create_upload_file(projectName: str, file: UploadFile, request):
@@ -49,7 +110,7 @@ async def create_upload_file(projectName: str, file: UploadFile, request):
     df = pd.DataFrame(data).T  # Write in DF and transpose it
     df.columns = cols  # Update column names
     data = df.to_json(orient='split')
-    print(data)
+    # print(data)
     return {"data": data}
 
 @app.post("/uploadJsonfile/")
@@ -58,7 +119,7 @@ async def create_upload_file(projectName: str, file: UploadFile, request):
     path = Path('D:/aryabhatta/training' + '/Regression/' + projectName + '/data/' + ts + '_' + file.filename)
     await save_upload_file(file, path)
     df = open(path, 'r').read()
-    print(df)
+    # print(df)
     return {df}
 
 @app.post("/uploadfile/")
@@ -68,7 +129,6 @@ async def create_upload_file(projectName: str, file: UploadFile, request):
     await save_upload_file(file, path)
     return {"filename": file.filename}
 
-
 async def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
     try:
         upload_file.file.seek(0)
@@ -76,7 +136,6 @@ async def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
             shutil.copyfileobj(upload_file.file, buffer)
     finally:
         upload_file.file.close()
-
 
 if __name__ == '_main_':
     uvicorn.run(app, host="0.0.0.0", port=8888)
